b12179b6ad7f626a9ed76106a7ac8903b58feb2a
Breyanna Vass <breyannavass@gmail.com>
2020-02-25 14:35:05 -0500

    making a transparent background

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 PIXI.utils.sayHello();
 
-var renderer = PIXI.autoDetectRenderer(512, 512, {
-  transparent: true,
-  resolution: 1
-});
+var renderer = new PIXI.Application({ transparent: true });
 
 document.getElementById("display").appendChild(renderer.view);
 
-var stage = new PIXI.Container();
+// var stage = new PIXI.Container();
 
-PIXI.loader.add("images/pixie.jpg").load(setup);
+// PIXI.loader.add("images/pixie.jpg").load(setup);
 
-var pixie;
+// var pixie;
 
-function setup() {
-  pixie = new PIXI.Sprite(PIXI.loader.resources["images/pixie.jpg"].texture);
+// function setup() {
+//   pixie = new PIXI.Sprite(PIXI.loader.resources["images/pixie.jpg"].texture);
 
-  stage.addChild(pixie);
-  renderer.render(stage);
-}
+//   pixie.scale.set(0.5, 0.5);
+//   pixie.x = renderer.width / 2;
+//   pixie.y = renderer.height / 2;
+//   pixie.anchor.set(0.5, 0.5);
+
+//   stage.addChild(pixie);
+//   renderer.render(stage);
+// }
+
+// Unrelated but important for code challenge: https://codepen.io/SkuliOskarsson/pen/ZbJKVW
+
+// Suppose to be a transparent background
+const pixie = PIXI.Sprite.from("images/pixie.jpg");
+
+pixie.anchor.set(0.5);
+
+pixie.x = renderer.screen.width / 2;
+pixie.y = renderer.screen.height / 2;
+
+renderer.stage.addChild(pixie);
+
+add.ticker.add(() => {
+  pixie.rotation += 0.1;
+});
