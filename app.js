a14b44a721229fd49c2125c0b98b40d93725d530
Breyanna Vass <breyannavass@gmail.com>
2020-02-25 14:01:39 -0500

    changing the image

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+PIXI.utils.sayHello();
+
+var renderer = PIXI.autoDetectRenderer(512, 512, {
+  transparent: true,
+  resolution: 1
+});
+
 document.getElementById("display").appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-PIXI.loader.add("images/pixi.png").load(setup);
+PIXI.loader.add("images/pixie.jpg").load(setup);
 
 var pixie;
 
 function setup() {
-  pixie = new PIXI.Sprite(PIXI.loader.resources["images/pixi.png"].texture);
+  pixie = new PIXI.Sprite(PIXI.loader.resources["images/pixie.jpg"].texture);
 
   stage.addChild(pixie);
   renderer.render(stage);
